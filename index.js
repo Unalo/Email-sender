461d7b3d935d224245dd399eee38999b806d511a
unalo <unalomtshokotsha@gmail.com>
2020-11-23 12:12:21 +0200

    changed router

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use(app.router)
+
 app.use("/api/", appRoute);
 
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
